64d9c46995b44c5996928788c6e0ffd5705fc412
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-18 10:40:56 +0600

    added react marquee

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,69 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>This is Navbar</h1>
+  const navLinks = (
+    <>
+      <Link to="/">
+        <li>
+          <a>Home</a>
+        </li>
+      </Link>
+      <Link to="/career">
+        <li>
+          <a>Career</a>
+        </li>
+      </Link>
+      <Link to="/news">
+        <li>
+          <a>News</a>
+        </li>
+      </Link>
+    </>
+  );
+  return (
+    <div className="navbar shadow-lg">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navLinks}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost text-xl">Dragon News</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar online">
+          <div className="w-12 rounded-full me-1">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+        <Link to="/login">
+          <a className="btn btn-sm">Login</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
