30935b8c3304952b3f50cc9cd76802036116922a
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-18 08:04:55 +0600

    added shared components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h1>This is Home</h1>
+            
         </div>
     );
 };
---
+++ src/Pages/Shared/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                this is header
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
