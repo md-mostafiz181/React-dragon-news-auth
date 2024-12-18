4a2753bbfa21625314210a39335f755d4d15ebb6
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-18 08:30:11 +0600

    added dragon header

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
@@ -7,7 +9,17 @@ const Home = () => {
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h1>This is Home</h1>
+            <div className="grid md:grid-cols-4 grid-cols-1 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h1>News coming soon</h1>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
             
         </div>
     );
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,10 +2,11 @@
 
 const Header = () => {
     return (
-        <div>
-            <h1>
-                this is header
-            </h1>
+        <div className="text-center">
+            <h1 className="text-3xl font-bold  py-3">The Dragon News</h1>
+            <p className="text-2xl font-bold">Its a highly authentic news portal</p>
+               
+            
         </div>
     );
 };
