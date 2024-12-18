6cf3f4a5364ce1b82671c461abc31386d1659b95
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-18 11:01:58 +0600

    added react icon for creating azaming button

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar shadow-lg">
+    <div className="navbar">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,20 @@
-
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const RightSideNav = () => {
-    return (
-        <div>
-           <h1>This is right side nav</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center">Login With</h1>
+      <button className="btn w-full">
+        <FcGoogle />
+        Login With Google
+      </button>
+      <button className="btn w-full mt-1">
+        <FaGithub />
+        Login With Github
+      </button>
+    </div>
+  );
 };
 
 export default RightSideNav;
