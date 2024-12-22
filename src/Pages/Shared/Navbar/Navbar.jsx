55716a7d2dcd888a7ff60efae422594f2c7048e1
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-22 07:15:22 +0600

    added login page

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -18,6 +18,11 @@ const Navbar = () => {
           <a>News</a>
         </li>
       </Link>
+      <Link to="/login">
+        <li>
+          <a>Login</a>
+        </li>
+      </Link>
     </>
   );
   return (
