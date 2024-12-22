6618f6d71737f7c8e7717dde4807322f30734f43
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-22 10:59:27 +0600

    show news details and show dynamic news details

--- src/Pages/Home/NewsCard.jsx
+++ src/Pages/Home/NewsCard.jsx
@@ -14,7 +14,7 @@ const NewsCard = ({news}) => {
   <div className="card-body">
     <h2 className="card-title">{title}</h2>
     {
-        details.length > 200 ? <p>{details.slice(0,200)} <Link to={`/news/${news._id}`} className="text-blue-600 font-bold">Read More</Link> </p>: <p>{details}</p>
+        details.length > 200 ? <p>{details.slice(0,200)} <Link to={`/news/${_id}`} className="text-blue-600 font-bold">Read More</Link> </p>: <p>{details}</p>
     }
     
   </div>
--- src/Pages/Home/NewsDetails.jsx
+++ src/Pages/Home/NewsDetails.jsx
@@ -1,9 +1,22 @@
 
-const NewsDetails = ({news}) => {
-    const {title}=news;
+import { useParams } from "react-router-dom";
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const NewsDetails = () => {
+    const {id} = useParams();
+ 
+
+
+
+  console.log(id)
     return (
         <div>
-            <h1>News Details :{title}</h1>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h1>News Details: {id}</h1>
+
         </div>
     );
 };
